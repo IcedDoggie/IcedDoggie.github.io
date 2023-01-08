3a7a0ce73529cc9c640881f187a3172e04543879
ice <hqkhor95@gmail.com>
2023-01-08 16:16:49 +0800

    moved posts to _posts subdir

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,40 @@ var store = [{
         "tags": [],
         "url": "/2023/01/02/dataScience.html",
         "teaser": null
+      },{
+        "title": "Tackling Upstream Task to Perform Downstream ME Classification",
+        "excerpt":"test  ","categories": ["labNotes"],
+        "tags": [],
+        "url": "/labnotes/2023/01/02/SSLME.html",
+        "teaser": null
+      },{
+        "title": "Domain Adaptation",
+        "excerpt":"test  ","categories": ["research"],
+        "tags": [],
+        "url": "/research/2023/01/02/domainAdaptation.html",
+        "teaser": null
+      },{
+        "title": "Hyperbolic CV with Domain Adaptation",
+        "excerpt":"test  ","categories": ["labNotes"],
+        "tags": [],
+        "url": "/labnotes/2023/01/02/hyperbolicCV.html",
+        "teaser": null
+      },{
+        "title": "Image Emotion Classification",
+        "excerpt":"test  ","categories": ["research"],
+        "tags": [],
+        "url": "/research/2023/01/02/imageEmotion.html",
+        "teaser": null
+      },{
+        "title": "Facial Micro-Expression",
+        "excerpt":"test  ","categories": ["research"],
+        "tags": [],
+        "url": "/research/2023/01/02/microExpression.html",
+        "teaser": null
+      },{
+        "title": "The process of Ideation and Creation of Face Analysis Pipeline",
+        "excerpt":"test  ","categories": ["industry"],
+        "tags": [],
+        "url": "/industry/2023/01/02/pipelineCreation.html",
+        "teaser": null
       }]
